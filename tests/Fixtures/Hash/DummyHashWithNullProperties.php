4c7fc454dbeecc21575504a439b6db7db3cd53fe
clementtalleu <clement.talleu@gmail.com>
2024-07-02 11:53:23 +0200

    hash null ok

--- tests/Fixtures/Hash/DummyHashWithNullProperties.php
+++ tests/Fixtures/Hash/DummyHashWithNullProperties.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Talleu\RedisOm\Tests\Fixtures\Json;
+namespace Talleu\RedisOm\Tests\Fixtures\Hash;
 
 use Talleu\RedisOm\Om\Mapping as RedisOm;
 use Talleu\RedisOm\Om\RedisFormat;
 use Talleu\RedisOm\Tests\Fixtures\AbstractDummy;
 
-#[RedisOm\Entity(format: RedisFormat::JSON->value)]
-class DummyJsonWithNullProperties extends AbstractDummy
+#[RedisOm\Entity]
+class DummyHashWithNullProperties extends AbstractDummy
 {
     #[RedisOm\Property(index: true)]
     public ?string $unknown = null;
