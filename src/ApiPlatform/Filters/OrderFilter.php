eac74671edb7b9de0b3212b4587fc80677cff335
pierre.perez <pierre.perez@cbainfo.fr>
2026-01-14 10:00:41 +0100

    fix: API Platform 4.2 change order properties

--- src/ApiPlatform/Filters/OrderFilter.php
+++ src/ApiPlatform/Filters/OrderFilter.php
@@ -15,23 +15,41 @@ class OrderFilter extends RedisAbstractFilter
     public function __invoke(array $params, ?Parameter $parameter = null, array $context = []): array
     {
         // todo: use constraint validations instead
-        if (!is_array($parameter->getValue())) {
+        if (is_null($parameter)) {
             return $params;
         }
 
-        foreach ($parameter->getValue([]) as $prop => $value) {
-            if (!in_array($prop, $this->properties, true)) {
-                continue;
+        $property = $parameter->getProperty();
+        $value = $parameter->getValue();
+
+        // API Platform 4.1 : getValue() return an array ["age" => "ASC"]
+        if (is_array($value)) {
+            foreach ($value as $prop => $direction) {
+                if (!in_array($prop, $this->properties, true)) {
+                    continue;
+                }
+
+                if (!in_array(strtoupper($direction), ['ASC', 'DESC'], true)) {
+                    continue;
+                }
+
+                $params['orderBy'][$prop] = strtoupper($direction);
             }
 
-            // could also use validation constraints
-            if (!in_array(strtoupper($value), ['ASC', 'DESC'])) {
-                continue;
-            }
-
-
-            $params['orderBy'][$prop] = strtoupper($value);
+            return $params;
         }
+
+
+        // API Platform 4.2+ : getValue() return a string "ASC" et getProperty() return "age"
+        if (!in_array($property, $this->properties, true)) {
+            return $params;
+        }
+
+        if (!is_string($value) || !in_array(strtoupper($value), ['ASC', 'DESC'], true)) {
+            return $params;
+        }
+
+        $params['orderBy'][$property] = strtoupper($value);
 
         return $params;
     }
